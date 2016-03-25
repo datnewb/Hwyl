ca3fa9ae86c6cc6f9f20ace0355429d7013a6628
Maynard Lopez <nod32lopez@gmail.com>
2016-03-25 22:31:53 +0800

    player movement
    -added player movement based on where the player taps/clicks

--- Assets/Scripts/Adventure/PlayerMovement.cs
+++ Assets/Scripts/Adventure/PlayerMovement.cs
@@ -2,5 +2,31 @@
 
 public class PlayerMovement : MonoBehaviour
 {
+    public bool m_shouldCameraFollow;
+    public LayerMask m_interactMask;
 
+    private NavMeshAgent m_navMeshAgent;
+
+    void Start()
+    {
+        m_navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+        {
+            Vector2 screenClickPoint = Vector2.zero;
+            if (Input.touchCount > 0)
+                screenClickPoint = Input.GetTouch(0).position;
+            else
+                screenClickPoint = Input.mousePosition;
+            Ray cameraRay = Camera.main.ScreenPointToRay(new Vector3(screenClickPoint.x, screenClickPoint.y, 0));
+            RaycastHit hitInfo;
+            if (Physics.Raycast(cameraRay, out hitInfo, 100, m_interactMask))
+            {
+                m_navMeshAgent.SetDestination(hitInfo.point);
+            }
+        }
+    }
 }
