9d59363c1f109710fd1edcfc99338fbe749e621b
Maynard Lopez <nod32lopez@gmail.com>
2016-05-07 08:23:38 +0800

    dialog system early prototype
    -added prototype interactable prefab to test room 1
    -added prototype dialog system

---
+++ Assets/Scripts/Adventure/DialogHandler.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogHandler : MonoBehaviour
+{
+    [SerializeField]
+    internal GameObject m_dialogPanel;
+    [SerializeField]
+    internal GameObject m_dialogNamePanel;
+
+    private bool m_canGoNext = false;
+    internal bool m_isInDialog = false;
+
+    private Interactible m_objectInteracting = null;
+    private PlayerMovement m_playerMovement = null;
+
+    void Start()
+    {
+        m_dialogPanel.SetActive(false);
+        m_playerMovement = FindObjectOfType<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        if (m_canGoNext)
+        {
+            if (Input.GetMouseButtonDown(0) || Input.touchCount > 0)
+            {
+                EndDialog();
+            }
+        }
+    }
+
+    internal void StartDialog(Interactible interactible)
+    {
+        m_playerMovement.m_canMove = false;
+
+        m_dialogPanel.SetActive(true);
+        if (interactible.m_interactibleType == InteractibleType.Object)
+            m_dialogNamePanel.SetActive(false);
+        else
+            m_dialogNamePanel.SetActive(true);
+        m_isInDialog = true;
+        m_objectInteracting = interactible;
+        m_canGoNext = true;
+    }
+
+    internal void EndDialog()
+    {
+        m_dialogPanel.SetActive(false);
+        m_canGoNext = false;
+        Invoke("DialogReset", 0.1f);
+    }
+
+    private void DialogReset()
+    {
+        m_playerMovement.m_canMove = true;
+
+        m_objectInteracting.m_isInteracting = false;
+        m_isInDialog = false;
+    }
+
+    private void AllowNext()
+    {
+        m_canGoNext = true;
+    }
+}
---
+++ Assets/Scripts/Adventure/Interactible.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public enum InteractibleType
+{
+    Person,
+    Object
+}
+
+public class Interactible : MonoBehaviour
+{
+    [SerializeField]
+    internal InteractibleType m_interactibleType;
+
+    private GameObject m_playerObject;
+    internal bool m_isInteracting;
+
+    private DialogHandler m_dialogHandler;
+
+    void Start()
+    {
+        m_playerObject = GameObject.FindGameObjectWithTag("Player");
+        m_isInteracting = false;
+
+        m_dialogHandler = FindObjectOfType<DialogHandler>();
+    }
+
+    void Update()
+    {
+        if (m_isInteracting)
+        {   
+            if (Vector3.Distance(transform.position, m_playerObject.transform.position) < 3)
+            {
+                if (!m_dialogHandler.m_isInDialog)
+                    m_dialogHandler.StartDialog(this);
+            }
+        }
+
+        //check if this object was clicked/touched
+        if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+        {
+            Vector2 screenClickPoint = Vector2.zero;
+            if (Input.touchCount > 0)
+                screenClickPoint = Input.GetTouch(0).position;
+            else
+                screenClickPoint = Input.mousePosition;
+            Ray cameraRay = Camera.main.ScreenPointToRay(new Vector3(screenClickPoint.x, screenClickPoint.y, 0));
+            RaycastHit hitInfo;
+            if (Physics.Raycast(cameraRay, out hitInfo, 100))
+            {
+                if (hitInfo.collider.transform.root == transform)
+                {
+                    m_isInteracting = true;
+                }
+                else
+                {
+                    m_isInteracting = false;
+                }
+            }
+        }
+    }
+}
