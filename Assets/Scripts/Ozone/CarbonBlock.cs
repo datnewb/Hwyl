0412814f9ceb0ff7be045474f1c49f16eaca8234
Maynard Lopez <nod32lopez@gmail.com>
2016-05-21 20:01:57 +0800

    ozone carbon block spawning and destroying
    -added ozone scene
    -created carbon block spawning and destruction

---
+++ Assets/Scripts/Ozone/CarbonBlock.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+public enum CarbonBlockType
+{
+    Downward,
+    Upward
+}
+
+public class CarbonBlock : MonoBehaviour
+{
+    internal float m_speed;
+    internal CarbonBlockType m_carbonBlockType;
+
+    void Update()
+    {
+        float speed = 0;
+        if (m_carbonBlockType == CarbonBlockType.Downward)
+            speed = -m_speed;
+        else
+            speed = m_speed;
+
+        GetComponent<Rigidbody2D>().MovePosition((Vector2)transform.position + new Vector2(0, speed * Time.deltaTime));
+    }
+}
